e90360d8077fc5e50cae6ad147336bcd476491e4
hyi <hongyi@renci.org>
2022-08-28 19:02:12 -0400

    update normalize inference curies code to handle real data

--- inference/normalize_inference_curies.py
+++ inference/normalize_inference_curies.py
@@ -10,13 +10,13 @@ warnings.filterwarnings('ignore', '.*ssl*', )
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments.')
     parser.add_argument('--input_file', type=str,
-                        default='../data/pubmed_split_11_prediction_output.csv',
+                        default='../data/annotated_17687_output.csv',
                         help='input model prediction file that contains predicted MESH IDs')
     parser.add_argument('--MESH_ID_COLUMN_NAME', type=str,
                         default='Predicted MESH ID',
                         help='MESH ID column name in the input file to read in')
     parser.add_argument('--output_file', type=str,
-                        default='../data/normalized_split_11_predictions.json',
+                        default='../data/annotated_17687_output_biolink.csv',
                         help='output file that contains output from node normalizer to each input MESH ID')
 
 
@@ -25,10 +25,11 @@ if __name__ == '__main__':
     mesh_id_col = args.MESH_ID_COLUMN_NAME
     output_file = args.output_file
 
-    input_df = pd.read_csv(input_file, usecols=[mesh_id_col])
-    print(input_df)
+    input_df = pd.read_csv(input_file)
+    print(input_df.shape)
     input_df[mesh_id_col] = 'MESH:' + input_df[mesh_id_col]
     input_list = list(input_df[mesh_id_col])
+    print(input_list)
     node_normalizer_url = 'https://nodenormalization-sri.renci.org/1.2/get_normalized_nodes'
     post_data = {
         "curies": input_list,
@@ -36,6 +37,7 @@ if __name__ == '__main__':
     }
     result = requests.post(node_normalizer_url, json=post_data)
     print(result.status_code)
-    # json_object = json.dumps(dictionary, indent=4)
-    with open(output_file, "w") as outf:
-        json.dump(result.json(), outf, indent=2)
+    result_dict = result.json()
+    print(result_dict)
+    input_df['Predicted Category'] = input_df[mesh_id_col].map(lambda x: result_dict[x])
+    input_df.to_csv(output_file, index=False)
