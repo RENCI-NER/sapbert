c33f54454179c8863e2c783f78bf626744a377df
hyi <hongyi@renci.org>
2022-09-26 16:45:20 -0400

    added more detailed comparison

--- inference/compare_prediction.py
+++ inference/compare_prediction.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import argparse
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments.')
     # parser.add_argument('--input_file', type=str, default='../data/pubmed_split_11_prediction_output.csv',
@@ -10,17 +9,35 @@ if __name__ == '__main__':
     # parser.add_argument('--column_2_name', type=str, default='Predicted MESH ID', help='second column to compare')
     parser.add_argument('--input_file', type=str, default='../data/annotated_17687_output_biolink.csv',
                         help='input file for comparison between prediction columns')
-    parser.add_argument('--column_1_name', type=str, default='Labelled Category', help='first column to compare')
-    parser.add_argument('--column_2_name', type=str, default='Predicted Category', help='second column to compare')
+    parser.add_argument('--input_text_column_name', type=str, default='Span text', help='input text column name')
+    parser.add_argument('--column_1_name', type=str, default='Biomegatron predicted type',
+                        help='first column to compare')
+    parser.add_argument('--column_2_name', type=str, default='Sapbert predicted type', help='second column to compare')
     parser.add_argument('--list_comparision', type=bool, default=False, help='whether to do list comparison or not')
+    parser.add_argument('--diff_output_file', type=str, default='../data/comparison/diff_predicted_types.csv',
+                        help='output file name for different prediction breakdown')
+    parser.add_argument('--same_null_output_file', type=str, default='../data/comparison/same_null_predicted_types.csv',
+                        help='output file name for same null predictions')
+    parser.add_argument('--same_not_null_output_file', type=str,
+                        default='../data/comparison/same_not_null_predicted_types.csv',
+                        help='output file name for same not null predictions')
 
     args = parser.parse_args()
     input_file = args.input_file
+    input_text_column_name = args.input_text_column_name
     column_1_name = args.column_1_name
     column_2_name = args.column_2_name
     list_comarison = args.list_comparision
+    diff_output_file = args.diff_output_file
+    same_null_output_file = args.same_null_output_file
+    same_not_null_output_file = args.same_not_null_output_file
 
-    df = pd.read_csv(input_file, usecols=[column_1_name, column_2_name], dtype=str)
+    df = pd.read_csv(input_file, usecols=[input_text_column_name, column_1_name, column_2_name], dtype=str)
+    print(f'before droping duplicates: {df.shape}')
+    df.drop_duplicates(inplace=True)
+    print(f'after droping duplicates: {df.shape}')
+    df = df[df['Span text'].isna() == False]
+    df[column_2_name].fillna('biolink:NamedThing', inplace=True)
     if list_comarison:
         df['Prediction_agreement'] = df.apply(lambda x: x[column_2_name] in x[column_1_name], axis=1)
     else:
@@ -30,8 +47,21 @@ if __name__ == '__main__':
     disagree_df = df[df.Prediction_agreement == False]
     if not list_comarison:
         # get percentage of empty column_1_name and column_2_name
-        col_1_null = df[column_1_name].isna()
-        col_2_null = df[column_2_name].isna()
-        print(f'{column_1_name} null percentage: {len(col_1_null.loc[lambda x: x == True])/len(df)}')
-        print(f'{column_2_name} null percentage: {len(col_2_null.loc[lambda x: x == True])/len(df)}')
-    print(f'agreement percentage: {len(agree_df)/len(df)}')
+        col_1_null = df[df[column_1_name] == 'biolink:NamedThing']
+        col_2_null = df[df[column_2_name] == 'biolink:NamedThing']
+        col_12_null = df[(df[column_1_name] == 'biolink:NamedThing') & (df[column_2_name] == 'biolink:NamedThing')]
+        col_12_same_not_null = df[(df[column_1_name] == df[column_2_name]) &
+                                  (df[column_1_name] != 'biolink:NamedThing') &
+                                  (df[column_2_name] != 'biolink:NamedThing')]
+        print(f'{column_1_name} biolink:NamedThing percentage: {len(col_1_null) / len(df)}')
+        print(f'{column_2_name} biolink:NamedThing percentage: {len(col_2_null) / len(df)}')
+        print(f'Both {column_1_name} and {column_2_name} biolink:NamedThing percentage: {len(col_12_null) / len(df)}')
+        diff_df = df[df['Prediction_agreement'] == False].sort_values(by=[column_1_name, column_2_name])
+        diff_df.drop(columns=['Prediction_agreement'], inplace=True)
+        diff_df.to_csv(diff_output_file, index=False)
+        col_12_null.drop(columns=['Prediction_agreement']).sort_values(by=[input_text_column_name]).to_csv(
+            same_null_output_file, index=False)
+        col_12_same_not_null.drop(columns=['Prediction_agreement']).sort_values(by=[input_text_column_name]).to_csv(
+            same_not_null_output_file, index=False)
+
+    print(f'agreement percentage: {len(agree_df) / len(df)}, disagreement percentage: {len(disagree_df) / len(df)}')
---
+++ inference/comparison_summary.md
@@ -0,0 +1,8 @@
+## Comparison of predicted types between Biomegatron model and SapBERT model for the split 11 subset of 17687 PUBMED abstracts
+- Percent of predicted biolink:NamedThing types (i.e., null type returned from node normalizer) from biomegatron is `50.42%`
+- Percent of predicted biolink:NamedThing types from sapBERT is `47.80%`
+- Percent of same predicted biolink:NamedThing types from both models is `34.95%`
+- Percent of same predicted types including null biolink:NamedThing types from both models is `54.38%`
+- Percent of same predicted types excluding null biolink:NamedThing types is `19.43%`
+- Percent of different predicted types from two models is `45.62%`
+- Breakdown type comparisons between two models can be found in `sapbert/data/comparison` directory.
