f04223c4ff268999b7d3d917161248b51332bfc7
hyi <hongyi@renci.org>
2022-10-25 21:45:16 -0400

    added script for combining two data sources and dropping biolink type column

---
+++ training_data/babel/create_training_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process arguments.')
+    parser.add_argument('--input_file_name_1', type=str,
+                        default='/projects/babel/sapbert-training/2022oct13/compendia/AnatomicalEntity.txt',
+                        help='the first input file to drop the first column and combine with the second input file')
+    parser.add_argument('--input_file_name_2', type=str,
+                        default='/projects/babel/sapbert-training/2022oct13/compendia/GrossAnatomicalStructure.txt',
+                        help='the second input file to drop the first column and combine with the first input file')
+    parser.add_argument('--output_file', type=str,
+                        default='/projects/ner/software/sapbert/sapbert/data/babel/anatomy.txt',
+                        help='output file to combine two input files while dropping the first column to create '
+                             'initial sapbert training data')
+
+    args = parser.parse_args()
+    input_file_name_1 = args.input_file_name_1
+    input_file_name_2 = args.input_file_name_2
+    output_file = args.output_file
+
+    df1 = pd.read_csv(input_file_name_1, sep='\|\|', header=None, usecols=[1, 2, 3])
+    df2 = pd.read_csv(input_file_name_2, sep='\|\|', header=None, usecols=[1, 2, 3])
+    df = pd.concat([df1, df2], ignore_index=True)
+    # since pandas does not support multiple character separator, cannot directly use to_csv to
+    # write data frame to csv with separator ||
+    row_series = df[df.columns].astype(str).apply(lambda x: '||'.join(x), axis=1)
+    row_series.to_csv(output_file, index=False)
