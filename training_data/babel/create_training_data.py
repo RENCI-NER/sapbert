642745f7420ad0c0ac847e404c0f61484fa384bd
hyi <hongyi@renci.org>
2024-02-13 16:20:31 -0500

    updated create training data script to remove synonym pairs with same names with case-insensitive comparison

--- training_data/babel/create_training_data.py
+++ training_data/babel/create_training_data.py
@@ -5,13 +5,17 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments.')
     parser.add_argument('--input_file_dir', type=str,
-                        default='/projects/babel/babel-outputs/2024jan9/sapbert-training-data',
+                        # default='/projects/babel/babel-outputs/2024jan9/sapbert-training-data',
+                        default='/home/hongyi/sapbert/data/babel/input',
                         help='input file directory to concatenate with input_file_list')
+    # parser.add_argument('--input_file_list', type=list, default=[
+    #     'AnatomicalEntity.txt', 'GrossAnatomicalStructure.txt', 'Cell.txt',
+    #     'CellularComponent.txt', 'BiologicalProcess.txt', 'Pathway.txt', 'MolecularActivity.txt',
+    #     'PhenotypicFeature.txt', 'Disease.txt', 'umls.txt', 'OrganismTaxon.txt',
+    #     'Gene.txt', 'Protein.txt', 'DrugChemicalConflated.txt', 'GeneFamily.txt'
+    # ], help='input file list to process')
     parser.add_argument('--input_file_list', type=list, default=[
-        'AnatomicalEntity.txt', 'GrossAnatomicalStructure.txt', 'Cell.txt',
-        'CellularComponent.txt', 'BiologicalProcess.txt', 'Pathway.txt', 'MolecularActivity.txt',
-        'PhenotypicFeature.txt', 'Disease.txt', 'umls.txt', 'OrganismTaxon.txt',
-        'Gene.txt', 'Protein.txt', 'DrugChemicalConflated.txt', 'GeneFamily.txt'
+        'GeneFamily.txt'
     ], help='input file list to process')
     parser.add_argument('--output_dir', type=str,
                         default='/projects/ner/software/sapbert/sapbert/data/babel/2024_v2',
@@ -25,7 +29,14 @@ if __name__ == '__main__':
     for f in input_file_list:
         # the columns of the input data are biolink curie || id || name || name1 || name2 where name is the
         # canonical label to create name to id pairs for sapbert predictions
-        df = pd.read_csv(os.path.join(input_file_dir, f), sep='\|\|', header=None, usecols=[1, 3, 4], engine='python')
+        df = pd.read_csv(os.path.join(input_file_dir, f), sep='\|\|', header=None,
+                         usecols=[1, 3, 4], names=["ID", "NAME1", "NAME2"], engine='python')
+        df['NAME1'] = df['NAME1'].apply(lambda x: x.strip().lower())
+        df['NAME2'] = df['NAME2'].apply(lambda x: x.strip().lower())
+
+        # filter out those rows where the synonym pairs are the same with case-insensitive comparison
+        df = df[df.NAME1 != df.NAME2]
+
         # since pandas does not support multiple character separator, cannot directly use to_csv to
         # write data frame to csv with separator ||
         row_series = df[df.columns].astype(str).apply(lambda x: '||'.join(x), axis=1)
