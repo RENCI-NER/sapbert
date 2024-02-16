687a2f9027c3cb2b7012a5156d3b90d48688bffe
hyi <hongyi@renci.org>
2024-02-16 09:51:46 -0500

    updated pretrain.sh

--- train/pretrain.sh
+++ train/pretrain.sh
@@ -1,13 +1,16 @@
-CUDA_VISIBLE_DEVICES=$1 python3 train.py \
-	--model_dir "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext" \
-	--train_dir $PATH_TO_TRAIN_FILE \
-	--output_dir tmp/sapbert\
+#!/bin/bash
+export PYTHONPATH=/data/code/train
+CUDA_VISIBLE_DEVICES=0 python3 /data/code/train/train.py \
+	--model_dir "/data/SapBERT-from-PubMedBERT-fulltext" \
+	--train_dir "/babeldata/training_data/" \
+	--output_dir "/data/SapBERT-fine-tuned-babel" \
+	--logging_file "/data/SapBERT-fine-tuned-babel/train_log.txt" \
 	--use_cuda \
 	--epoch 1 \
 	--train_batch_size 256 \
 	--learning_rate 2e-5 \
 	--max_length 25 \
-	--checkpoint_step 999999 \
+	--checkpoint_step 17300 \
 	--parallel \
 	--amp \
 	--pairwise \
